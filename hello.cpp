30e6fc84df838fdd3d334a3415c583e5af3f7cda
mrnajych <mrnajych@gmail.com>
2023-10-31 12:45:01 +0530

    quick sort by branch quicksort

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 using namespace std;
+void quicksort(){
+    //// aslfnaslfkasnfnaslnflalsdknfsnlfknaslfnaslkflkkndsfladlfnsdlfnaslkfasdlk
+}
 int main(){
 cout <<"Hello World";
 cout <<"\n india is the best"<<endl;
